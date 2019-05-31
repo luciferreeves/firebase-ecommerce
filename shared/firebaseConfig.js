c2be1c94a41cdce6dda9a71c2ed966ee8f00b47c
Kumar Priyansh <bobbyskhs@gmail.com>
2019-05-31 16:55:34 +0700

    Firebase Config Added

---
+++ shared/firebaseConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB268bKD89cJROouDrZv-Jrih8B7H6Rq5c",
+    authDomain: "luciferecommerce.firebaseapp.com",
+    databaseURL: "https://luciferecommerce.firebaseio.com",
+    projectId: "luciferecommerce",
+    storageBucket: "luciferecommerce.appspot.com",
+    messagingSenderId: "410863129095",
+    appId: "1:410863129095:web:da1cf3c361723a7a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
