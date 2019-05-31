27760f1655b7fa1d2413bee75d26b4a95f20c3e4
Kumar Priyansh <bobbyskhs@gmail.com>
2019-05-31 18:35:27 +0700

    Updated Dashboard JS to display products according to URL

--- shared/Dashboard.js
+++ shared/Dashboard.js
@@ -21,4 +21,37 @@ $(document).ready(function(){
          window.location.replace('../')
         }
     });
+    // var url = window.location.href;
+    // 
+    
+    var url = window.location.href;
+    if( url.search( 'productid' ) > 0 ) {
+        option = url.match(/productid=(.*)/)[1];
+        $.getJSON("../products.json", function(result){
+            var size = (Object.keys(result).length)
+            if(option > -1 && option < size) {
+                loadProduct(option);
+            }
+            else {
+                showErrorDiv()
+            }
+        })
+    }  else {
+        loadAllProducts()
+    }
+    function loadProduct (productid) {
+        
+    }
+    function showErrorDiv() {
+
+    }
+    function loadAllProducts() {
+        $.getJSON("../products.json", function(result){
+            var size = (Object.keys(result).length)
+            for(i = 0; i < size ; i++)
+            {
+                // $('#loader')
+            }
+        });
+    }
 })
