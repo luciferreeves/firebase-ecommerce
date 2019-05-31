2c03a82cecd99b72fb6a4d010cd17ae9493817ab
Kumar Priyansh <bobbyskhs@gmail.com>
2019-05-31 17:12:09 +0700

    Updated  Login JS

--- shared/Login.js
+++ shared/Login.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
+    function showAlert(message) {
+        // Get the snackbar DIV
+        var x = document.getElementById("snackbar");
+        $('#snackbar').text(message)
+        // Add the "show" class to DIV
+        x.className = "show";
+      
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+      }
     $('#login').click(function(){
         var email = $("#email").val()
         var password = $("#password").val()
         if(email === '' || password === '') {
-            alert('Please fill in all the fields')
+            showAlert('Please fill in all the fields')
         } else {
             firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
                 // Handle Errors here.
                 var errorCode = error.code;
                 var errorMessage = error.message;
-                alert(errorMessage)
+                showAlert(errorMessage)
                 // ...
             });
         }
