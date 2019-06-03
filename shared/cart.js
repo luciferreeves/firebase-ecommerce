7b8b70eb20a23600da5d6082927d3b9aa4d7c8c0
Kumar Priyansh <bobbyskhs@gmail.com>
2019-06-03 06:13:20 +0530

    Added Signout function and Auto Redirect for Non Authenicated users

--- shared/cart.js
+++ shared/cart.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-    
+    $('#cart').click(function(){
+        window.location.replace('../')
+    })
+    function showAlert(message) {
+        // Get the snackbar DIV
+        var x = document.getElementById("snackbar");
+        $('#snackbar').text(message)
+        // Add the "show" class to DIV
+        x.className = "show";
+      
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    }
+    $('#signout').click(function(){
+        firebase.auth().signOut().then(function() {
+            window.location.replace('../')
+        }).catch(function(error) {
+            showAlert("Failed to sign out")
+        });
+    })
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+         window.location.replace('../')
+        }
+    });
 })
