c6e8231b317c3104f49222b1628f2d7287de13a8
Kumar Priyansh <bobbyskhs@gmail.com>
2019-06-09 19:44:47 +0530

    Login Functions Updated

--- shared/js/Login.js
+++ shared/js/Login.js
@@ -5,23 +5,26 @@ $(document).ready(function(){
         $('.toast').toast('show')
     }
     $('#login').click(function(){
+        $('#login').html('&emsp;<div class="spinner-grow spinner-grow-sm text-light" role="status"><span class="sr-only">Loading...</span></div>&emsp;');
         var email = $("#email").val()
         var password = $("#password").val()
         if(email === '' || password === '') {
-            showAlert('Error', 'Please fill in all the fields')
+            $('#login').html('Login')
+            showAlert('Sign in Error', 'Email or Password empty. You need to fill in all the details to continue with login. Please fill in all details and retry.')
         } else {
             firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+                $('#login').html('Login')
                 // Handle Errors here.
                 var errorCode = error.code;
                 var errorMessage = error.message;
-                showAlert('Error',errorMessage)
+                showAlert('Sign in Error',errorMessage)
                 // ...
             });
         }
     })
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-         window.location.replace('dashboard')
+         window.location.replace('../')
         }
     });
 })
