7485c7aca1e7135ccb74201a0e5e5cf75eecbdef
Kumar Priyansh <bobbyskhs@gmail.com>
2019-06-09 19:52:53 +0530

    Updated Login and Logged in Functions

--- shared/js/Dashboard.js
+++ shared/js/Dashboard.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-    function showAlert(message) {
-        // Get the snackbar DIV
-        var x = document.getElementById("snackbar");
-        $('#snackbar').text(message)
-        // Add the "show" class to DIV
-        x.className = "show";
-      
-        // After 3 seconds, remove the show class from DIV
-        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-    }
     $('#signout').click(function(){
         firebase.auth().signOut().then(function() {
             window.location.replace('../')
@@ -17,8 +7,10 @@ $(document).ready(function(){
         });
     })
     firebase.auth().onAuthStateChanged(function(user) {
-        if (!user) {
-         window.location.replace('../')
+        if (user) {
+            console.log(user)
+            $("#sign").hide()
+            $("#signed").show()
         }
     });
     // var url = window.location.href;
